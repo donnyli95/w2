793cfda313fbba50c3caf24b3037f7ead497f9a8
donnyli95 <sz3li@uwaterloo.ca>
2021-04-05 21:48:07 +0000

    refreshing skills

---
+++ d1/titleCase/test/titleCaseTest.js
@@ -0,0 +1,32 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+const titleCase = require('../titleCase');
+
+describe('titleCase', () => {
+
+  it('titleCase("this is an example") should return "This Is An Example"', () => {
+
+  });
+
+  it('titleCase("test") should return "Test"', () => {
+
+  });
+
+  it('titleCase("i r cool") should return "I R Cool"', () => {
+
+  });
+
+  it('titleCase("WHAT HAPPENS HERE") should return "What Happens Here"', () => {
+
+  });
+
+  it('titleCase("") should return ""', () => {
+
+  });
+
+  it('titleCase("A") should return "A"', () => {
+
+  });
+
+});
