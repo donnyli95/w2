7ab06e87eeb5a2478b47bf6bfdc73711585ea1b7
donnyli95 <sz3li@uwaterloo.ca>
2021-04-06 19:08:04 +0000

    async programming

---
+++ d2/typewriter/typewriter.js
@@ -0,0 +1,10 @@
+const sentence = "hello there from lighthouse labs";
+
+let time = 0;
+for (let char of sentence) {
+  setTimeout(() => {process.stdout.write(char);}, time, time += 50);
+};
+
+setTimeout(() => {process.stdout.write('\n');}, 50 * sentence.length);
+
+
