d18fed13b5ee51bbbc3271ae1ffd986edcff8afe
donnyli95 <sz3li@uwaterloo.ca>
2021-04-08 22:09:52 +0000

    linting and testing

--- d4-api/json_the_cat/breedFetcher.js
+++ d4-api/json_the_cat/breedFetcher.js
@@ -2,23 +2,28 @@ const request = require('request');
 
 const webpage = 'https://api.thecatapi.com/v1/breeds/search?q=';
 
-const quer = process.argv[2];
+const fetchBreedDescription = function(breedName, callback) {
+
+  request(webpage + breedName, (error, response, body) => {
+    if (error) {
+      callback(error);
+    } else {
+
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        callback("error", null);
+      } else {
+        callback(null, data[0].description);
+      }
+    }
 
 
-request(webpage + quer, (error, response, body) => {
-  if (error) {
-    console.log('error', error); // print if error occurs
-    console.log('statusCode', response && response.statusCode); // Print the response status code
-  }
- 
-  const data = JSON.parse(body);
+  
+  });
+  
+};
 
-  if (data.length < 1) {
-    console.log("I think you made a mistake");
-  } else {
-    console.log(data[0].description);
-    console.log(typeof data);
-  }
+module.exports = { fetchBreedDescription };
 
 
-});
+
